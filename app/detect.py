b02598d83b0bc160b71130fb4aeb08899f741bd2
andrzejom <30194965+andrzejom@users.noreply.github.com>
2021-02-19 16:00:28 +0100

    Update detect.py

--- app/detect.py
+++ app/detect.py
@@ -15,23 +15,28 @@ if key:
     plt.title('Pearson Correlation of Features', y=1.05, size=15)
     sns.heatmap(train_dataset.corr(), linewidths=0.1, vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
     plt.show()
+    
 #Dropping less relevant data
 train_dataset_ref = train_dataset.drop(['PassengerId','Cabin'],axis=1)
-#print(train_dataset_ref.shape)
+
+
 #Changing data representation for Sex
 train_dataset_ref['Sex']=train_dataset_ref.loc[ train_dataset_ref['Sex'] == 'male', 'Sex'] = 0
 train_dataset_ref['Sex']=train_dataset_ref.loc[ train_dataset_ref['Sex'] == 'female', 'Sex'] = 1
 train_dataset_ref['Sex']=train_dataset_ref['Sex'] = train_dataset_ref['Sex'].astype(int)
+
 #To simply the data representation mapping age to 3 categories
 train_dataset_ref['Age']=train_dataset_ref.loc[ train_dataset_ref['Age'] <= 18, 'Age'] = 0
 train_dataset_ref['Age']=train_dataset_ref.loc[(train_dataset_ref['Age'] > 18) & (train_dataset_ref['Age'] <= 54), 'Age'] = 1
 train_dataset_ref['Age']=train_dataset_ref.loc[ train_dataset_ref['Age'] > 54, 'Age'] = 2
+
 #To simply the data representation mapping fare to 4 categories
 train_dataset_ref['Fare']=train_dataset_ref.loc[ train_dataset_ref['Fare'] <= 10, 'Fare'] = 0
 train_dataset_ref['Fare']=train_dataset_ref.loc[(train_dataset_ref['Fare'] > 10) & (train_dataset_ref['Fare'] <= 50), 'Fare'] = 1
 train_dataset_ref['Fare']=train_dataset_ref.loc[(train_dataset_ref['Fare'] > 50) & (train_dataset_ref['Fare'] <= 100), 'Fare'] = 2
 train_dataset_ref['Fare']=train_dataset_ref.loc[ train_dataset_ref['Fare'] > 100, 'Fare'] = 3
 train_dataset_ref['Fare']=train_dataset_ref['Fare'] = train_dataset_ref['Fare'].astype(int)
+
 x = train_dataset_ref[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']]
 y = train_dataset_ref[['Survived']]
 
